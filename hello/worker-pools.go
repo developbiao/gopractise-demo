90fd295d11e37f554f5e9564eed9555b695bbcf6
developbiao <developbiao@gmail.com>
2021-08-16 17:20:24 +0800

    implement worker pools demo

--- hello/worker-pools.go
+++ hello/worker-pools.go
@@ -1,17 +1,36 @@
 package main
 
-import "fmt"
-import "time"
-
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-    jobs := make(chan int, 100) 
-    results := make(chan int, 100)
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
 
-    for w := 1; w <= 3; w++ {
-    }
+	for w := 1; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
+
+	// send 9 jobs after close
+	for j := 1; j <= 9; j++ {
+		jobs <- j
+	}
+	close(jobs)
+
+	// Collection results
+	for a := 1; a <= 9; a++ {
+		<-results
+	}
 
 }
 
-
-
+// Worker process job from channel
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for j := range jobs {
+		fmt.Println("worker", id, "processing job", j)
+		time.Sleep(time.Second)
+		results <- j * 2
+	}
+}
