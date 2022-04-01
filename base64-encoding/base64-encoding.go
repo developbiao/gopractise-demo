347bcc781d38d333c654b1d3568e66624780309b
developbiao <developbiao@gmail.com>
2022-04-01 17:08:00 +0800

    clear using wls2

---
+++ base64-encoding/base64-encoding.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	b64 "encoding/base64"
+	"fmt"
+)
+
+func main() {
+	data := "abc123!?$*&()'-=@~"
+
+	// Base64 encoding
+	sEnc := b64.StdEncoding.EncodeToString([]byte(data))
+	fmt.Println(sEnc)
+
+
+	// Base64 decoding
+	sDec, _ := b64.StdEncoding.DecodeString(sEnc)
+	fmt.Println(string(sDec))
+	fmt.Println()
+
+
+	uEnc := b64.URLEncoding.EncodeToString([]byte(data))
+	fmt.Println(uEnc)
+	uDec, _ := b64.URLEncoding.DecodeString(uEnc)
+	fmt.Println(string(uDec))
+
+}
+
